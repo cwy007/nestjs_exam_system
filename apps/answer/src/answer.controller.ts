3fb04326fba115e22b6eebc7802273dd25ea8309
chanweiyan <chanweiyan007@gmail.com>
2026-05-25 12:20:21 +0800

    fix: 测试微服务调用是否成功

--- apps/answer/src/answer.controller.ts
+++ apps/answer/src/answer.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AnswerService } from './answer.service';
+import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
 
 @Controller()
 export class AnswerController {
-  constructor(private readonly answerService: AnswerService) {}
+  constructor(private readonly answerService: AnswerService) { }
+
+  @Inject('EXAM_SERVICE')
+  private readonly examClient: ClientProxy;
 
   @Get()
-  getHello(): string {
-    return this.answerService.getHello();
+  async getHello(): Promise<string> {
+    const pattern = 'sum';
+    const data = [1, 2, 3, 4, 5];
+    const value = await firstValueFrom(this.examClient.send<number>(pattern, data));
+    return this.answerService.getHello() + value;
   }
 }
--- apps/exam/src/exam.controller.ts
+++ apps/exam/src/exam.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { ExamService } from './exam.service';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class ExamController {
-  constructor(private readonly examService: ExamService) {}
+  constructor(private readonly examService: ExamService) { }
 
   @Get()
   getHello(): string {
     return this.examService.getHello();
+  }
+
+  @MessagePattern('sum')
+  async accumulate(data: number[]): Promise<number> {
+    return (data || []).reduce((a, b) => a + b);
   }
 }
